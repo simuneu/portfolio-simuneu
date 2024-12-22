b0058c139829a8d528ff34356e5175b2d34d4ff9
simuneu <worksimu@gmail.com>
2024-12-22 22:29:11 +0900

    엔티티 개발2

--- src/main/kotlin/com/simune/portfolio/domain/entity/ExperienceDetail.kt
+++ src/main/kotlin/com/simune/portfolio/domain/entity/ExperienceDetail.kt
@@ -3,9 +3,22 @@ package com.simune.portfolio.domain.entity
 import jakarta.persistence.*
 
 @Entity
-class ExperienceDetail : BaseEntity() {
+class ExperienceDetail(
+    content: String,
+    isActive: Boolean
+) : BaseEntity() {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "experience_detail_id")
     var id: Long? = null //?은 null을 허용
+
+    var content: String = content
+
+    var isActive: Boolean = isActive
+
+    fun update(content: String, isActive: Boolean) {
+        this.content = content
+        this.isActive = isActive
+    }
 }
